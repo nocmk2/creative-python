c484f98e4ac2f98fdd09ada0b1615ca0e6ca76f0
zhangmk <51670196@qq.com>
2020-06-19 23:20:36 +0800

    010 update

--- 010.py
+++ 010.py
@@ -1,1 +1,6 @@
+if __name__ == "__main__":
+    a = ["B", "NULL", "ABA", "CC", "NULL", "CA"]
+    i = filter(lambda x: not x.startswith("C"), a)
+    res = list(i)
+    print(res)  # ['B', 'ABA', 'CC', 'CA']
 
