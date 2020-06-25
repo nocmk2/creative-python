913ab5491fb7de1442fbdbe851d403fa96fd8c92
zhangmk <51670196@qq.com>
2020-06-25 16:47:58 +0800

    更新准备工作

--- play.py
+++ play.py
@@ -1,18 +1,2 @@
-import re
-
 if __name__ == "__main__":
-    rich = [
-        "Tom$$$",
-        "Jack$$$$$$$",
-        "Aris$$$$$$$",
-        "Jay$$",
-        "Tom$$$$$$$$$",
-        "Jerry$$$",
-    ]
-    the_richest = sorted(
-        rich, key=lambda x: len(re.sub(r"[^$]", "", x)), reverse=True
-    )
-    print(
-        the_richest
-    )  # ['Tom$$$$$$$$$', 'Jack$$$$$$$', 'Aris$$$$$$$', 'Tom$$$', 'Jerry$$$', 'Jay$$']
-
+    print("Hello world")
