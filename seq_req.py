519832573da9317b24e52d7a893da3691c7799fe
nocmk2 <51670196@qq.com>
2020-09-04 15:18:07 +0800

    flags

--- seq_req.py
+++ seq_req.py
@@ -3,3 +3,47 @@ import time
 import sys
 
 import requests
+
+POP20_CC = ("CN IN US ID BR PK NG BD RU JP MX PH VN ET EG DE IR TR CD FR").split()
+
+# 这个站关闭了，我现在按照https://github.com/fluentpython/example-code/blob/master/17-futures-py3.7/countries/README.rst搭建一个本地的服务器
+BASE_URL = "http://flupy.org/data/flags"
+
+DEST_DIR = "downloads/"
+
+
+def save_flag(img, filename):
+    path = os.path.join(DEST_DIR, filename)
+    with open(path, "wb") as fp:
+        fp.write(img)
+
+
+def get_flag(cc):
+    url = "{}/{cc}/{cc}.gif".format(BASE_URL, cc=cc.lower())
+    resp = requests.get(url)
+    return resp.content
+
+
+def show(text):
+    print(text, end=" ")
+    sys.stdout.flush()
+
+
+def download_many(cc_list):
+    for cc in sorted(cc_list):
+        image = get_flag(cc)
+        show(cc)
+        save_flag(image, cc.lower() + ".gif")
+
+    return len(cc_list)
+
+def main(download_many):
+    t0 = time.time()
+    count = download_many(POP20_CC)
+    elapsed = time.time() - t0
+    msg = '\n{} flags dowoaded in {:.2f}s'
+    print(msg.format(count, elapsed))
+
+if __name__ == "__main__":
+    main(download_many)
+
