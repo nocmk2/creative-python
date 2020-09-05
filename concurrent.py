1c5b468a5e1756b77b693b1f9140ffdbb21d6dc2
zhangmk <51670196@qq.com>
2020-09-06 07:51:51 +0800

    add concurrent.Futures ThreadPoolExecutor

---
+++ concurrent.py
@@ -0,0 +1,76 @@
+import os
+import time
+import sys
+
+import requests
+
+from concurrent import futures
+
+POP20_CC = (
+    "CN IN US ID BR PK NG BD RU JP MX PH VN ET EG DE IR TR CD FR".split()
+)
+
+BASE_URL = "http://localhost:8021/"
+
+DEST_DIR = "downloads/"
+MAX_WORKERS = 20
+
+
+def save_flag(img, filename):
+    path = os.path.join(DEST_DIR, filename)
+    with open(path, "wb") as fp:
+        fp.write(img)
+
+
+def get_flag(cc):
+    url = "{}/{cc}/{cc}.gif".format(BASE_URL, cc=cc.lower())
+    resp = requests.get(url)
+    return resp.content
+
+
+def show(text):
+    print(text, end=" ")
+    sys.stdout.flush()
+
+
+def download_one(cc):
+    image = get_flag(cc)
+    show(cc)
+    save_flag(image, cc.lower() + ".gif")
+    return cc
+
+
+def download_many_seq(cc_list):
+    for cc in sorted(cc_list):
+        download_one(cc)
+
+    return len(cc_list)
+
+
+def download_many_futures(cc_list):
+    workers = max(MAX_WORKERS, len(cc_list))
+    with futures.ThreadPoolExecutor(workers) as executor:
+        res = executor.map(download_one, cc_list)
+
+    return len(list(res))
+
+
+def main():
+    t0 = time.time()
+    count = download_many_seq(POP20_CC)
+    elapsed = time.time() - t0
+    msg = "\n{} flags downloaded by sequence in {:.6f}s"
+    print(msg.format(count, elapsed))
+
+    t1 = time.time()
+    count = download_many_futures(POP20_CC)
+    elapsed = time.time() - t1
+    msg = "\n{} flags downloaded by ThreadPoolExecutor in {:.6f}s"
+    print(msg.format(count, elapsed))
+
+    # Deferred class in Twisted
+    # Future 类类似Twisted类中的Deferred类 Tornado中的Future类 JS中的Promise对象
+
+
+if __name__ == "__main__":
+    main()
