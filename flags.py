288652a2f2289eec60430c11f37f7ef86306385a
nocmk2 <51670196@qq.com>
2020-09-07 14:48:53 +0800

    rename

--- flags.py
+++ flags.py
@@ -54,6 +54,25 @@ def download_many_futures(cc_list):
 
     return len(list(res))
 
+def download_many_submit(cc_list):
+    cc_list = cc_list[:5]
+    with futures.ThreadPoolExecutor(max_workers=3) as executor:
+        to_do = []
+        for cc in sorted(cc_list):
+            future = executor.submit(download_one, cc)
+            to_do.append(future)
+            msg = 'Scheduled for {}: {}'
+            print(msg.format(cc, future))
+
+        results = []
+        for future in futures.as_completed(to_do):
+            res = future.result()
+            msg = '{} result: {!r}'
+            print(msg.format(future, res))
+            results.append(res)
+    
+    return len(results)
+
 
 def main():
     t0 = time.time()
@@ -68,6 +87,11 @@ def main():
     msg = "\n{} flags downloaded by ThreadPoolExecutor in {:.6f}s"
     print(msg.format(count, elapsed))
 
+    t2 = time.time()
+    count = download_many_submit(POP20_CC)
+    elapsed = time.time() - t2
+    msg = "\n{} flags downloaded by ThreadPoolExecutor submit in {:.6f}s"
+    print(msg.format(count, elapsed))
     # Deferred class in Twisted
     # Future 类类似Twisted类中的Deferred类 Tornado中的Future类 JS中的Promise对象
 
